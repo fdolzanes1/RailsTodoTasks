8fefdb499f86a147cf236193dd3b827aac103371
fdolzanes1 <fdolzanes1@gmail.com>
2019-11-28 21:00:27 -0400

    criando o Header

--- tasks_client/src/App.js
+++ tasks_client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import Header from './components/header/Header'
 import logo from './logo.svg';
 import './App.css';
+import './App.scss';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
@@ -10,20 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 2
-          </a>
-        </header>
+        <Header/>
       </div>
     );
   }
