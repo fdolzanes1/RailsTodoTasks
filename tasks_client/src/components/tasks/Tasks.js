d9552b92f5aaa220ff1e4866273a0007db4e5ccd
fdolzanes1 <fdolzanes1@gmail.com>
2019-11-30 13:20:35 -0400

    criando componente de listagem

--- tasks_client/src/components/tasks/Tasks.js
+++ tasks_client/src/components/tasks/Tasks.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import List from './list/List';
 
-export class Tasks extends Component {
+class Tasks extends Component {
   render() {
     return (
       <Row>
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">To-do</p>
-          List...
+          <List/>
         </Col>
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">Done</p>
-          List...
+          <List/>
         </Col>
       </Row>
     );
