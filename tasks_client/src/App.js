38d175e21933c85cf8025c23e046b306dfdd838e
fdolzanes1 <fdolzanes1@gmail.com>
2019-11-30 13:08:29 -0400

    criando componente principal das Tasks

--- tasks_client/src/App.js
+++ tasks_client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import Header from './components/header/Header'
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Header from './components/header/Header';
+import Tasks from './components/tasks/Tasks';
 import './App.scss';
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
@@ -11,8 +12,11 @@ library.add(faCheckCircle, faTrashAlt)
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Header/>
+        <Container>
+          <Tasks/>
+        </Container>
       </div>
     );
   }
